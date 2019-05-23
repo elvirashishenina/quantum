b12aecf39bdf819cd7fc03d6e69008af42f5f784
Elvira Shishenina <ella.shishenina@gmail.com>
2019-05-23 15:49:21 +0200

    problem 6-2

---
+++ 6_Boolean_func_and_Q_gates/prob6-2/prob6-2/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  prob6-2
+//
+// boolean functions & quantum gates
+//  Created by Elvira on 23/05/2019.
+//  Copyright © 2019 Elvira.Shishenina. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
